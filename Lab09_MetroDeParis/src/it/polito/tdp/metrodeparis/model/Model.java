7a2d06f75f140f7b5de4b66fcbbc0839f85a60b0
Arianna <Arianna@pc-arianna.homenet.telecomitalia.it>
2017-05-09 22:10:38 +0200

    Interfaccia grafica e popolamento combobox

--- Lab09_MetroDeParis/src/it/polito/tdp/metrodeparis/model/Model.java
+++ Lab09_MetroDeParis/src/it/polito/tdp/metrodeparis/model/Model.java
@@ -1,5 +1,13 @@
 package it.polito.tdp.metrodeparis.model;
 
+import java.util.List;
+
+import it.polito.tdp.metrodeparis.dao.MetroDAO;
+
 public class Model {
 
+	public List<Fermata> getAllFermate(){
+		MetroDAO dao = new MetroDAO();
+    	return dao.getAllFermate();
+	}
 }
